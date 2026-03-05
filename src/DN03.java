356b1c9192c453372ba7030c326a9358564f7509
Matic Štucin <matic.stucin@gmail.com>
2026-03-05 21:12:28 +0100

    Create DN03.java

---
+++ src/DN03.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Random;
+import java.util.Scanner;
+
+public class DN03 {
+    public static void main(String[] args)throws Exception{
+        args = new String[]{"gesla.txt", "10", "100"};
+        Scanner sc = new Scanner(new File(args[0]));
+
+        int n = sc.nextInt(); sc.nextLine();
+        String[] besede = new String[n];
+
+
+        
+        for (int i = 0; i < n; i++) {
+            besede[i] = sc.nextLine();
+        }
+
+
+        String geslo = "";
+        Random rnd = new Random(Integer.parseInt(args[2]));
+        for (int i = 0; i<Integer.parseInt(args[1]);i++){
+            String naklj_beseda = besede[rnd.nextInt(besede.length)];
+            char nakljucna_crka = naklj_beseda.charAt(rnd.nextInt(naklj_beseda.length()));
+            geslo += nakljucna_crka;
+
+        }
+        System.out.println(geslo);
+
+
+    }
+}
