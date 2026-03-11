b3f4275bb100b84becff7ac52f1f88a43035dd10
Matic Štucin <matic.stucin@gmail.com>
2026-03-11 09:32:09 +0100

    Add Kviz2 with digit-sum and suffix check

---
+++ src/Kviz2.java
@@ -0,0 +1,39 @@
+public class Kviz2 {
+    static int vsotaStevk(String str){
+        int vsota = 0;
+        for (int i = 0; i<str.length(); i++){
+            char znak = str.charAt(i);
+            if(Character.isDigit(znak)){
+                vsota += Integer.parseInt(String.valueOf(znak));
+            }
+        }
+        return vsota;
+    }
+
+    static boolean preveriRep(String a, String b){
+        String[] besede = a.split(" ");
+
+
+        String rep;
+        if (a.length() > b.length()){
+            rep = a.substring(a.length() - b.length(), a.length()).toLowerCase();
+            if (b.toLowerCase().equals(rep)){
+                return true;
+            }
+
+        }
+        else {
+            rep = b.substring(b.length() - a.length(), b.length()).toLowerCase();
+            if (a.toLowerCase().equals(rep)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(vsotaStevk("1a2c"));
+
+    }
+}
