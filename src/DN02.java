19e33e374f2eb119636e23b8569a04ee303f0d56
Matic Štucin <matic.stucin@gmail.com>
2026-03-27 13:00:17 +0100

    Update DN02.java

--- src/DN02.java
+++ src/DN02.java
@@ -1,21 +1,18 @@
-public class DN02 {
+void main(String[] args) {
+    if (args.length == 0) {
+        return;
+    }
+    int stranica = (int) Math.ceil(Math.sqrt(args[0].length()));
+    int st_znakov = (stranica * stranica);
+    for (int i = 0; i < st_znakov; i++) {
 
-    public static void main(String[] args){
-        if(args.length == 0){
-            return;
+        if (i < args[0].length()) {
+            IO.print(" " + args[0].charAt(i) + " "); // dodani presledki so samo za "olepševanje" izgleda kvadrata
+        } else {
+            IO.print(" . ");
         }
-        int stranica = (int) Math.ceil(Math.sqrt(args[0].length()));
-        int st_znakov = (stranica * stranica);
-        for (int i=0; i<st_znakov; i++){
-
-            if (i<args[0].length()){
-                System.out.print(" " + args[0].charAt(i) + " "); // dodani presledki so samo za "olepševanje" izgleda kvadrata
-            } else {
-                System.out.print(" . ");
-            }
-            if ((i+1) % stranica == 0){
-                System.out.print('\n');
-            }
+        if ((i + 1) % stranica == 0) {
+            IO.print('\n');
         }
     }
 }
