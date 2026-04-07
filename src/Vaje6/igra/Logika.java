4af112bf3ff794a2b918c144a8d3518bc7df4ce1
Matic Štucin <matic.stucin@gmail.com>
2026-04-07 23:06:28 +0200

    Create Logika.java

---
+++ src/Vaje6/igra/Logika.java
@@ -0,0 +1,30 @@
+package Vaje6.igra;
+
+public class Logika {
+    // definiranje spremenljivk potrebnih za delovanje igre
+    static int[][] polja;
+    static int tocke;
+
+    // metode za delovanje igre
+    public static void zacniNovoIgro(int velikost){
+        return;
+    }
+    public static void koncajIgro(){
+        return;
+    }
+    public static int vrniPloscico(int i, int j){
+        return 0;
+    }
+    public static int vrniTocke(){
+        return 0;
+    }
+    public static boolean jeZmagal(){
+        return false;
+    }
+    public static boolean jeKonec(){
+        return false;
+    }
+    public static void naslednjaPoteza(int smer){
+        return;
+    }
+}
