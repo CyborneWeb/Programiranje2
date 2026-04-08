83cd9554e37e271bddc9d2a64c9c1677ca77ed84
Matic Štucin <matic.stucin@gmail.com>
2026-04-08 12:18:56 +0200

    Initialize board and add random tiles
    Add Random import and rnd field; implement zacniNovoIgro to allocate board, reset score and konec, and place two random tiles (90% chance 2, 10% chance 4). Implement koncajIgro to set konec, basic getters vrniPloscico and vrniTocke, jeZmagal to detect a 2048 tile, and jeKonec to return the game-over flag. Note: naslednjaPoteza remains unimplemented.

--- src/Vaje6/igra/Logika.java
+++ src/Vaje6/igra/Logika.java
@@ -1,32 +1,54 @@
 package Vaje6.igra;
 
+import java.util.Random;
+
 public class Logika {
     // definiranje spremenljivk potrebnih za delovanje igre
     static int[][] polja;
     static int tocke;
-
     private static boolean konec;
+    // Objekt Random
+    static Random rnd = new Random();
+
     // metode za delovanje igre
     public static void zacniNovoIgro(int velikost){
         polja = new int[velikost][velikost];
         tocke = 0;
         konec = false;
+        int dodanih = 0;
+        while (dodanih < 2){
+
+            int i = rnd.nextInt(velikost);
+            int j = rnd.nextInt(velikost);
+            if (polja[i][j] == 0){
+                polja[i][j] = (rnd.nextInt(10) < 9 ? 2 : 4);
+                dodanih++;
+            }
+
+        }
         return;
+
+
     }
     public static void koncajIgro(){
-        return;
+        konec = true;
     }
     public static int vrniPloscico(int i, int j){
-        return 0;
+        return polja[i][j];
     }
     public static int vrniTocke(){
-        return 0;
+        return tocke;
     }
     public static boolean jeZmagal(){
+        for (int[] vrstica : polja){
+            for (int polje : vrstica){
+                if (polje == 2048) return true;
+            }
+        }
         return false;
     }
     public static boolean jeKonec(){
-        return false;
+        return konec;
     }
     public static void naslednjaPoteza(int smer){
         return;
